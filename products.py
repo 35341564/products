1ab0d081539ec17fb9a1d622639c6bb6772959f6
David Lee <68041584+35341564@users.noreply.github.com>
2022-07-05 21:11:50 +0800

    Update products.py

--- products.py
+++ products.py
@@ -1,4 +1,13 @@
 shop_list = []
+
+with open('products.csv','r',encoding='utf-8')as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue #跳到下一迴圈，不會跳出
+        name,price = line.strip().split(',')
+        shop_list.append([name,price])
+print(shop_list)
+
 while True:
     shop_name = input('請輸入商品名稱: ')
     if shop_name == 'q':
@@ -6,3 +15,11 @@ while True:
     shop_price = input('請輸入商品價格: ')
     shop_list.append([shop_name,shop_price]) #在大清單加入小清單中的價格與物品
 print(shop_list)
+
+for p in shop_list:
+    print(p[0],'的價格為:',p[1])
+
+with open('products.csv','w',encoding='utf-8')as f:
+    f.write('商品,價格\n')
+    for p in shop_list:
+        f.write(p[0] + ',' + str(p[1] + '\n'))
